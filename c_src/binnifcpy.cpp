bd887107f5a246e9e4f2daf5f0101e820fc50311
Bikram Chatterjee <razorpeak@gmail.com>
2018-08-20 09:10:49 +0200

    buffer async reset test

--- c_src/binnifcpy.cpp
+++ c_src/binnifcpy.cpp
@@ -1,9 +1,10 @@
 #include <stdint.h>
 #include <string.h>
+#include <windows.h>
 
 #include "erl_nif.h"
 
-static ERL_NIF_TERM byte_array_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+static ERL_NIF_TERM clear_bin(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     argc = argc; // for unused variable warning
 
@@ -18,13 +19,93 @@ static ERL_NIF_TERM byte_array_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM 
     return enif_make_binary(env, &buffer);
 }
 
+static ERL_NIF_TERM get_self(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    ErlNifPid caller;
+    if(!enif_self(env, &caller))
+        return enif_raise_exception(
+            env, enif_make_atom(env, "badprocess")
+        );
+
+    return enif_make_pid(env, &caller);
+}
+
+static ERL_NIF_TERM async_process(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    ErlNifPid caller;
+    if(!enif_self(env, &caller))
+        return enif_raise_exception(
+            env, enif_make_atom(env, "badprocess")
+        );
+
+    Sleep(3000);
+
+    if(!enif_send(
+        env, &caller, NULL,
+        enif_make_atom(env, "from_async_process"))
+    ) return enif_raise_exception(
+            env, enif_make_atom(env, "send_failed")
+    );
+}
+
+static ERL_NIF_TERM async(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    ErlNifPid caller;
+    if(!enif_self(env, &caller))
+        return enif_raise_exception(
+            env, enif_make_atom(env, "badprocess")
+        );
+
+    return enif_schedule_nif(env, "async_process", ERL_NIF_DIRTY_JOB_IO_BOUND,
+                             async_process, argc, argv);
+}
+
+static ERL_NIF_TERM clear_bin_async_process(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    ErlNifPid caller;
+    if(!enif_self(env, &caller))
+        return enif_raise_exception(
+            env, enif_make_atom(env, "badprocess")
+        );
+
+    ErlNifBinary buffer;
+    if (!enif_inspect_binary(env, argv[0], &buffer))
+        return enif_make_badarg(env);
+
+    uint8_t* buf = (uint8_t*)(buffer.data);
+    for (int i = 0; i < buffer.size; ++i)
+        buf[i] = 0;
+
+    if(!enif_send(
+        env, &caller, NULL,
+        enif_make_atom(env, "from_clear_bin_async_process"))
+    ) return enif_raise_exception(
+            env, enif_make_atom(env, "send_failed")
+    );
+}
+
+static ERL_NIF_TERM clear_bin_async(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    ErlNifPid caller;
+    if(!enif_self(env, &caller))
+        return enif_raise_exception(
+            env, enif_make_atom(env, "badprocess")
+        );
+
+    return enif_schedule_nif(env, "clear_bin_async_process", ERL_NIF_DIRTY_JOB_IO_BOUND,
+                             clear_bin_async_process, argc, argv);
+}
+
 int upgrade(ErlNifEnv* env, void** priv_data, void** old_priv_data, ERL_NIF_TERM load_info)
 {
     return 0;
 }
 
 static ErlNifFunc nif_funcs[] = {
-    {"byte_array", 1, byte_array_nif}
+    {"clear_bin", 1, clear_bin},
+    {"get_self", 0, get_self},
+    {"async", 0, async},
+    {"clear_bin_async", 1, clear_bin_async}
 };
 
 ERL_NIF_INIT(binnifcpy, nif_funcs, NULL, NULL, upgrade, NULL)
--- src/binnifcpy.erl
+++ src/binnifcpy.erl
@@ -1,13 +1,19 @@
 -module(binnifcpy).
--export([byte_array/1]).
+-export([clear_bin/1, get_self/0, async/0, clear_bin_async/1]).
 
 -on_load(init/0).
 
 init() ->
     erlang:load_nif(priv_dir() ++ "/binnifcpy", 0).
 
-byte_array(_X) ->
-    exit(nif_library_not_loaded).
+-define(NOT_LOADED,
+    erlang:nif_error({not_loaded, [{module, ?MODULE}, {line, ?LINE}]})
+).
+
+clear_bin(_X) -> ?NOT_LOADED.
+clear_bin_async(_X) -> ?NOT_LOADED.
+get_self() -> ?NOT_LOADED.
+async() -> ?NOT_LOADED.
 
 priv_dir() ->
     case code:priv_dir(?MODULE) of
@@ -22,12 +28,46 @@ priv_dir() ->
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
-basic_test() ->
+self_test() ->
+    ?assertEqual(self(), get_self()).
+
+bin_test() ->
     List = lists:duplicate(5 * 1024 * 1024, 0),
     BinZero = list_to_binary(List),
     TestBin = << <<(rand:uniform(255))>> || _ <- List>>,
     ?assertNotEqual(BinZero, TestBin),
-    ?assertEqual(BinZero, byte_array(TestBin)),
+    ?assertEqual(BinZero, clear_bin(TestBin)),
     ?assertEqual(BinZero, TestBin).
+
+async_test()->
+    T = os:timestamp(),
+    async(),
+    receive
+        Term ->
+            Took = timer:now_diff(os:timestamp(), T),
+            ?assert(Took > 0),
+            ?assert(Took < 4 * 1000 * 1000),
+            ?assertEqual(from_async_process, Term)
+    after 5000 ->
+        throw(no_message_received)
+    end.
+
+async_bin_test()->
+    List = lists:duplicate(5 * 1024 * 1024, 0),
+    BinZero = list_to_binary(List),
+    TestBin = << <<(rand:uniform(255))>> || _ <- List>>,
+    T = os:timestamp(),
+    ?assertNotEqual(BinZero, TestBin),
+    clear_bin_async(TestBin),
+    receive
+        Term ->
+            Took = timer:now_diff(os:timestamp(), T),
+            ?assert(Took > 0),
+            ?assert(Took < 3 * 1000 * 1000),
+            ?assertEqual(from_clear_bin_async_process, Term),
+            ?assertEqual(BinZero, TestBin)
+    after 5000 ->
+        throw(no_message_received)
+    end.
 
 -endif.
